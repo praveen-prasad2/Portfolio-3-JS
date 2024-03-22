1e9d57caf8cbc4055e2c1a061d14b612cea787db
Praveen Prasad <praveenprasad2510@gmail.com>
2024-03-22 20:32:36 +0530

    Final Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,21 +33,21 @@ const Contact = forwardRef((props, ref) => {
 
     emailjs
       .send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Coinkraft",
           from_email: form.email,
-          to_email: "praveenprasad2510@gmail.com",
+          to_email: "hello@coinkraft.xyz",
           message: form.message,
         },
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you. We will get back to you as soon as possible,Coinkraft");
 
           setForm({
             name: "",
